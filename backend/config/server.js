5992ee02993253d9a823868bb002c611e08ebd13
Duong Nguyen <duong@kalpha.io>
2021-07-20 00:00:51 +0700

    Strapi x NextJS

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a11106e374e55041696e785a39e0fae7'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8d1f8026-a86a-4e62-851e-d85d1f2dc596'
+};
